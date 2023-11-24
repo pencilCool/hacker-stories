e25439fdc4d8c86cce4e185dd4c9ccf8982ca655
pencilCool <yhtangcoder@gmail.com>
2023-11-24 08:49:40 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,92 @@
-import { useState } from 'react'
 import './App.css'
-
-function getTitle(title) {
-  return title
+import { useState } from 'react'
+const Item = (props) => {
+  let {item} = props
+  console.log(item)
+  return (
+    <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+  )
 }
 
-function App() {
+const List = (props) =>  {
+    let { list } = props
+
+    return (<ul>
+      {list.map((item)=>(
+         <Item key={item.objectID} item = {item}/>
+      )
+      )}
+    </ul>)
+}
+ 
+
+const App = () =>  {
+  
+  const [searchTerm,setSearchTerm] = useState("");
+
+  const stories = [
+    {
+    title:'React',
+    url:'https://reactjs.org/',
+    author:'Jordan Walke',
+    num_comments:3, 
+    points:4,
+    objectID:0,
+  },
+  {
+    title:'Redux',
+    url:'https://redux.js.org/',
+    author:'Dan Abramov Andress Clark',
+    num_comments:2, 
+    points:5,
+    objectID:1,
+  }
+  ]
+
+
+
+  const handleSearch = (event) => {
+    console.log(event.target.value);
+    setSearchTerm(event.target.value);
+    // filter stories 
+  }
+
+  const searchedStories  = stories.filter(function(story) {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase());
+  })
+
 
   return (
     <>
       <div>
-        <h1>Hello </h1>
-        <label htmlFor='search'>Search:
-        <input id='searchx' type='text'></input> 
-        </label>
+        <h1> My Hacker Stories </h1>
+        <h2>{searchTerm}</h2>
+        <Search onSearch={handleSearch}/>
+        <hr/>
+        <List list={searchedStories}/>
       </div> 
     </>
   )
 }
+   
+
+const Search = (props)=> {
+  return (
+    <div>
+      <label htmlFor='search'>Search:
+       </label>
+      <input id='search' type='text' onChange={props.onSearch}></input> 
+    </div>
+  )
+}
+
+
 
 export default App
