8a344e0dadc4a0f7d783aafb774c703468b9585b
pencilCool <yhtangcoder@gmail.com>
2023-11-26 07:51:37 +0800

    form

--- src/App.jsx
+++ src/App.jsx
@@ -99,6 +99,17 @@ const storiesReducer = (state,action) => {
 
 }
 
+const SearchForm = ({searchTerm,onSearchInput,onSearchSubmit}) => (
+  <form onSubmit={onSearchSubmit}>
+  <InputWithLabel id="search" label='search' value={searchTerm} onInputChange={onSearchInput}>
+  <strong>Search:</strong>
+  </InputWithLabel>
+  <button type='submit' disabled={!searchTerm}>
+    Submit
+  </button>
+  </form>
+)
+
 const App = () =>  {
   const [searchTerm,setSearchTerm] = useStorageState("search","react")
   const [stories,dispatchStories] = useReducer(storiesReducer,{data:[],isLoading:false,isError:false})
@@ -110,8 +121,9 @@ const App = () =>  {
     // filter stories 
   }
 
-  const handleSearchSumbit = ()=> {
+  const handleSearchSumbit = (event)=> {
     setUrl(`${API_ENDPOINT}${searchTerm}`)
+    event.preventDefault();
   }
 
   const handleFetchStories = useCallback(async ()=>{
@@ -144,15 +156,11 @@ const App = () =>  {
     <>
       <div>
         <h1> My Hacker Stories </h1>
-        <InputWithLabel id="search" label='search' value={searchTerm} onInputChange={handleSearchInput}>
-        <strong>Search:</strong>
-        </InputWithLabel>
-        <button type='button' disabled={!searchTerm} onClick={handleSearchSumbit}>
-          Submit
-        </button>
-
+          <SearchForm searchTerm={searchTerm} 
+            onSearchInput={handleSearchInput}          
+            onSearchSubmit={handleSearchSumbit}
+          />
         <hr/>
-
         {stories.isError && <p>Something went wrong ...</p>}
         {stories.isLoading ? (<p>Loading...</p>) :(
         <List list={stories.data} onRemoveItem={handleRemoveStory} />
